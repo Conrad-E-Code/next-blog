f21dbb8f37ed04a331b90d845d5dfaace6e37d57
Conrad Etherington <conrad.etherington@gmail.com>
2023-09-08 23:08:27 -0600

    set favicon, begin lexical integration

---
+++ components/EditorContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditorContainer = () => {
+  return (
+    <div className='justify-center text-center bg-blue-600 mx-4 my-4'>
+    Editor Container
+    
+  </div>  )
+}
+
+export default EditorContainer
