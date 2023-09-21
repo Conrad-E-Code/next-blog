64289b06d3bbd619f4efb0941c94a24c07ccd976
Conrad Etherington <conrad.etherington@gmail.com>
2023-09-21 14:15:36 -0600

    install mongoose and define ConXBlog Model

---
+++ models/ConXBlog.js
@@ -0,0 +1,10 @@
+import { Schema, model, models } from "mongoose";
+
+const ConXBlogSchema = new Schema({
+    contentJSON: {
+        type: String,
+        required: [true, "Must have JSON content"]
+    }
+})
+
+const ConXBlog = models.ConXBlog || model("ConXBlog", ConXBlogSchema);
