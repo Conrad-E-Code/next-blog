eb39101219cc8dc392cef72a894ed7ff6d79f2dc
Conrad Etherington <conrad.etherington@gmail.com>
2023-09-21 15:00:08 -0600

    /blogs GET route connected to atlas

--- app/api/blogs/route.js
+++ app/api/blogs/route.js
@@ -1,1 +1,17 @@
 //Blogs get route
+import { connectToDB } from "/utils/db";
+import ConXBlog from "/models/ConXBlog";
+
+export const dynamic = 'force-dynamic';
+
+export const GET = async (req) => {
+    try {
+            await connectToDB();
+            const blogs = await ConXBlog.find({});
+            return new Response(JSON.stringify(blogs), {
+                status: 200,
+            });
+    } catch (error) {
+        console.log(error, "GET error pjms route");
+    }
+};
