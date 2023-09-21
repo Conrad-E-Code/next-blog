a516d92cac952663e44e12ae6ed48bb7ada2b86d
Conrad Etherington <conrad.etherington@gmail.com>
2023-09-21 14:16:32 -0600

    export model

--- models/ConXBlog.js
+++ models/ConXBlog.js
@@ -1,10 +1,12 @@
 import { Schema, model, models } from "mongoose";
 
 const ConXBlogSchema = new Schema({
-    contentJSON: {
-        type: String,
-        required: [true, "Must have JSON content"]
-    }
-})
+  contentJSON: {
+    type: String,
+    required: [true, "Must have JSON content"],
+  },
+});
 
 const ConXBlog = models.ConXBlog || model("ConXBlog", ConXBlogSchema);
+
+export default ConXBlog;
