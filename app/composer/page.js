4f63941af928960e8438caf56f518e2b922b7ad5
Conrad Etherington <conrad.etherington@gmail.com>
2023-09-21 20:55:47 -0600

    create NavBAr UI

---
+++ app/composer/page.js
@@ -0,0 +1,12 @@
+import EditorContainer from '@/components/EditorContainer'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <EditorContainer />
+    </div>
+  )
+}
+
+export default page
--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className=' bg-zinc-200 flex flex-col'>
-<EditorContainer />
+    <div className='bg-zinc-200 flex flex-col'>
+
     </div>
   )
 }
