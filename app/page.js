eab3017812b8b44b0bc23b70386156d65620731b
Conrad Etherington <conrad.etherington@gmail.com>
2023-09-21 18:07:30 -0600

    set up client post request for /blogs/new

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='h-[100vh] w-[100vw] bg-zinc-200 flex flex-col'>
+    <div className=' bg-zinc-200 flex flex-col'>
 <EditorContainer />
     </div>
   )
