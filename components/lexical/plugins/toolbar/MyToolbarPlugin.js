295331cdae57819f0f2d4fde2f5a9fcf05b9bf25
Conrad Etherington <conrad.etherington@gmail.com>
2023-09-28 12:03:02 -0600

    upgrade autosave feature to trigger 15 seconds after detected changes.

--- components/lexical/plugins/toolbar/MyToolbarPlugin.js
+++ components/lexical/plugins/toolbar/MyToolbarPlugin.js
@@ -4,60 +4,14 @@ import MyBannerToolbarPlugin from "@/components/lexical/plugins/toolbar/MyBanner
 import MyListToolbarPlugin from "./MyListToolbarPlugin";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import MyHeaderPlugin from "./MyHeaderPlugin";
-import {AiOutlineOrderedList, AiOutlineUnorderedList, AiOutlineFileImage, } from "react-icons/ai"
 import {BsCodeSlash} from "react-icons/bs"
 import {CgLink} from "react-icons/cg"
 import HistoryToolbarPlugin from "./HistoryToolbarPlugin";
 import InsertToolbarPlugin from "./InsertToolbarPlugin";
 
-// import {
-//   TableNode,
-//   TableCellNode,
-//   INSERT_TABLE_COMMAND,
-//   $insertTableRow,
-//   $insertTableColumn,
-// } from "@lexical/table";
-
 
 export default function MyToolbarPlugin() {
   const [editor] = useLexicalComposerContext();
-
-
-  function handleLoadEditorState() {
-    const myState = {
-      root: {
-        children: [
-          {
-            children: [
-              {
-                detail: 0,
-                format: 0,
-                mode: "normal",
-                style: "",
-                text: "howdy",
-                type: "text",
-                version: 1,
-              },
-            ],
-            direction: "ltr",
-            format: "",
-            indent: 0,
-            type: "paragraph",
-            version: 1,
-          },
-        ],
-        direction: "ltr",
-        format: "",
-        indent: 0,
-        type: "root",
-        version: 1,
-      },
-    };
-    editor.update(() => {
-      editor.setEditorState(editor.parseEditorState(myState));
-    });
-  }
-
 
   return (
     <div className="flex gap-1 w-fit mx-auto py-2">
@@ -71,11 +25,3 @@ export default function MyToolbarPlugin() {
     </div>
   );
 }
-
-
-// <FaArrowCircleLeft
-// onClick={prevSlide}
-// style={{color: Colors[textClr]}}
-// className={`z-40 cursor-pointer absolute left-[20px] top-[35px] ease-in-out duration-1000`}
-// size={50}
-// />
